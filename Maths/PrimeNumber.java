0db6ca3b7149f2ee2f28c675ea880c4a4cdd73fd
aniket-6234 <aniketashok1999@gmail.com>
2022-06-14 09:02:47 +0530

    prime number

---
+++ Maths/PrimeNumber.java
@@ -0,0 +1,24 @@
+public class PrimeNumber {
+
+  public static void main(String[] args) {
+
+    int n = 20;
+    for(int i = 1; i <= n; i++) {
+       System.out.println(i + " " + isPrime(i));
+    }  
+  }
+
+  static boolean isPrime (int n) {
+    if(n <= 1) {    // Edge case
+      return false;
+    }
+    int start = 2;
+    while (start * start <= n) {  // (start <= n/2) is also used for better understanding
+       if(n % start == 0) {
+        return false;
+       }
+        start++;
+    }
+    return true;
+  }
+}
