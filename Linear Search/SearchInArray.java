cb9a4b3ed6e238fbbae0f9107d45dd2262ece5e0
aniket-6234 <aniketashok1999@gmail.com>
2022-06-23 07:14:47 +0530

    Linear Search

--- Linear Search/SearchInArray.java
+++ Linear Search/SearchInArray.java
@@ -2,19 +2,18 @@ public class SearchInArray {
 
   // program to find element in an array
   static int linearSearch(int[] nums, int target) {
-    int ans = -1;
     if(nums.length == 0) {  //if arr is empty return -1
-      return ans;
+      return -1;
     }
 
     // if not, then go through all elements
     for(int i=0; i<nums.length; i++) {
       if(nums[i] == target) {
-        ans = i;
+        return i;
       }
     }
 
-    return ans;
+    return -1;
   }
   public static void main(String[] args) {
     int[] arr = {3, 6, 90, 23, 76, 45, 24};
